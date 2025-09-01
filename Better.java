49a668baadf1bc35a6f3a1540d6a6fd04eedb2cb
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-09-02 01:55:28 +0530

    Better solution of 3Sum

---
+++ Better.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Better {
+    public List<List<Integer>> threeSum(int[] nums) {
+        HashSet<List<Integer>> result = new HashSet<>();
+        for(int i=0;i<nums.length-1;i++){
+            HashSet<Integer> hs = new HashSet<>();
+            for(int j=i+1;j<nums.length;j++){
+                if(hs.contains(-(nums[i]+nums[j]))){
+                    ArrayList<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(-(nums[i]+nums[j]));
+                    Collections.sort(temp);
+                    result.add(temp);
+                    // left++;
+                    // right++;
+                }
+                else{
+                    hs.add(nums[j]);
+                    // j++;
+                    
+                }
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>(result);
+        return ans;
+    }
+}
